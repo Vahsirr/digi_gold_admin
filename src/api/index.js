a93909888b80aa60e11b1d31121380a491e6e1bc
Rishav Mukherjee <rishavmukherjee013@gmail.com>
2026-05-09 09:10:26 +0530

    changed the root url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://jewel-production-7efe.up.railway.app/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://digi-gold-backend.vercel.app/api';
 // const API_BASE_URL = 'http://localhost:3001/api'
 const api = axios.create({
   baseURL: API_BASE_URL,
